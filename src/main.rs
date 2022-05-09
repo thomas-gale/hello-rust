bd04ebedf24edc688f13ae15bec8c1402e214ff1
Thomas Gale <gale.thomasjames@gmail.com>
2022-05-09 15:16:42 +0100

    Experimenting with custom shader using new wgsl format.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-use bevy::prelude::*;
+use bevy::{
+    ecs::system::{lifetimeless::SRes, SystemParamItem},
+    pbr::MaterialPipeline,
+    prelude::*,
+    reflect::TypeUuid,
+    render::{
+        render_asset::{PrepareAssetError, RenderAsset},
+        render_resource::{
+            std140::{AsStd140, Std140},
+            BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout,
+            BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, Buffer,
+            BufferBindingType, BufferInitDescriptor, BufferSize, BufferUsages, ShaderStages,
+        },
+        renderer::RenderDevice,
+    },
+};
 
 fn main() {
     App::new()
@@ -7,8 +22,10 @@ fn main() {
             brightness: 1.0 / 5.0f32,
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(MaterialPlugin::<CustomMaterial>::default())
         .add_startup_system(set_window)
-        .add_startup_system(setup)
+        .add_startup_system(setup_model_lights)
+        .add_startup_system(setup_custom_material)
         .add_system(animate_light_direction)
         .run();
 }
@@ -19,10 +36,10 @@ fn set_window(mut windows: ResMut<Windows>) {
     window.set_resolution(500.0, 500.0);
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_model_lights(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_scene(asset_server.load("models/digger_v3.glb#Scene0"));
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
     const HALF_SIZE: f32 = 1.0;
@@ -44,6 +61,22 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 }
 
+fn setup_custom_material(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+) {
+    // cube
+    commands.spawn().insert_bundle(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
+        transform: Transform::from_xyz(0., 0., 0.),
+        material: materials.add(CustomMaterial {
+            color: Color::GREEN,
+        }),
+        ..default()
+    });
+}
+
 fn animate_light_direction(
     time: Res<Time>,
     mut query: Query<&mut Transform, With<DirectionalLight>>,
@@ -55,5 +88,89 @@ fn animate_light_direction(
             time.seconds_since_startup() as f32 * std::f32::consts::TAU / 10.0,
             -std::f32::consts::FRAC_PI_4,
         );
+    }
+}
+
+// This is the struct that will be passed to your shader
+#[derive(Debug, Clone, TypeUuid)]
+#[uuid = "f690fdae-d598-45ab-8225-97e2a3f056e0"]
+pub struct CustomMaterial {
+    color: Color,
+}
+
+#[derive(Clone)]
+pub struct GpuCustomMaterial {
+    _buffer: Buffer,
+    bind_group: BindGroup,
+}
+
+// The implementation of [`Material`] needs this impl to work properly.
+impl RenderAsset for CustomMaterial {
+    type ExtractedAsset = CustomMaterial;
+    type PreparedAsset = GpuCustomMaterial;
+    type Param = (SRes<RenderDevice>, SRes<MaterialPipeline<Self>>);
+    fn extract_asset(&self) -> Self::ExtractedAsset {
+        self.clone()
+    }
+
+    fn prepare_asset(
+        extracted_asset: Self::ExtractedAsset,
+        (render_device, material_pipeline): &mut SystemParamItem<Self::Param>,
+    ) -> Result<Self::PreparedAsset, PrepareAssetError<Self::ExtractedAsset>> {
+        let color = Vec4::from_slice(&extracted_asset.color.as_linear_rgba_f32());
+        let buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
+            contents: color.as_std140().as_bytes(),
+            label: None,
+            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+        });
+        let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
+            entries: &[BindGroupEntry {
+                binding: 0,
+                resource: buffer.as_entire_binding(),
+            }],
+            label: None,
+            layout: &material_pipeline.material_layout,
+        });
+
+        Ok(GpuCustomMaterial {
+            _buffer: buffer,
+            bind_group,
+        })
+    }
+}
+
+impl Material for CustomMaterial {
+    // When creating a custom material, you need to define either a vertex shader, a fragment shader or both.
+    // If you don't define one of them it will use the default mesh shader which can be found at
+    // <https://github.com/bevyengine/bevy/blob/latest/crates/bevy_pbr/src/render/mesh.wgsl>
+
+    // For this example we don't need a vertex shader
+    // fn vertex_shader(asset_server: &AssetServer) -> Option<Handle<Shader>> {
+    //     // Use the same path as the fragment shader since wgsl let's you define both shader in the same file
+    //     Some(asset_server.load("shaders/custom_material.wgsl"))
+    // }
+
+    fn fragment_shader(asset_server: &AssetServer) -> Option<Handle<Shader>> {
+        Some(asset_server.load("shaders/custom_material.wgsl"))
+    }
+
+    fn bind_group(render_asset: &<Self as RenderAsset>::PreparedAsset) -> &BindGroup {
+        &render_asset.bind_group
+    }
+
+    fn bind_group_layout(render_device: &RenderDevice) -> BindGroupLayout {
+        render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+            entries: &[BindGroupLayoutEntry {
+                binding: 0,
+                visibility: ShaderStages::FRAGMENT,
+                ty: BindingType::Buffer {
+                    ty: BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: BufferSize::new(Vec4::std140_size_static() as u64),
+                },
+                count: None,
+            }],
+            label: None,
+        })
     }
 }
